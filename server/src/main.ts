8120293ad04c88d7995565685bf976e0d4add9af
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-12 12:51:41 +0300

    Refactor server main.ts and package.json, and remove unused akash.ts file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,12 @@
 import "dotenv/config";
 import express from "express";
-import { sendDeploy, walletFromMnemonic } from "./akash";
 
 const app = express();
 const port = 3001;
 
-const mnemonic =
-  "tragic wife grace victory volume journey base dirt calm morning lizard camera";
-
 app.listen(port, async () => {
   try {
-    const wallet = await walletFromMnemonic(mnemonic);
-    const [account] = await wallet.getAccounts();
-    const deploy = await sendDeploy();
-
-    console.log(account, deploy);
+    console.log(`Server running on port ${port}`);
   } catch (error: any) {
     console.error("Error: " + error);
   }
