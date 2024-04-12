7015ee5b05443cd39c7c0432aa6b09e2b5e5ee13
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-12 16:22:03 +0300

    added supabase

--- server/src/routes/deploy.ts
+++ server/src/routes/deploy.ts
@@ -2,23 +2,27 @@ import { Router } from "express";
 import { stringify } from "yaml";
 import * as fs from "fs";
 import { bids } from "../assets/mock";
-import { chdir } from "process";
 import { exec } from "child_process";
+import { Bid } from "../type";
+import { createClient } from '@supabase/supabase-js'
 
 const router = Router();
 
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
   console.log("Deploy request received:", req.body);
   loadYAMLFile("src/assets/deploy.sdl.yaml");
+  await saveBidsToDB(bids);
   res.status(201).json(bids);
 });
 
-router.post("/accept", (req, res) => {
+
+router.post("/accept", async (req, res) => {
   const bidId = req.body.body.bidId;
   if (!bidId) {
     return res.status(400).send("bid ID is required");
   }
-
+  const dibs = await loadBidsFromDB();
+  console.log(dibs)
   exec(
      'node -e "console.log(\'Hello, World!\')"',
     (error, stdout, stderr) => {
@@ -47,3 +51,37 @@ async function loadYAMLFile(filePath: string): Promise<any> {
     throw error;
   }
 }
+
+async function saveBidsToDB(bids: Bid[]): Promise<void> {
+  try {
+    const supabase = createClient(process.env.SUPABASE_PROJECT_URL!, process.env.SERVICE_ROLE_KEY!)
+    const output = bids.map((bid) => {
+      return {
+        id: bid.escrow_account.id.xid,
+        json: bid,
+      };
+    });
+    const { error }  = await supabase.from('bids').upsert(output);
+    if (error) {
+      throw error;
+    }
+
+  } catch (error) {
+    console.error("Error saving bids to database:", error);
+    throw error;
+  }
+}
+
+async function loadBidsFromDB(): Promise<{ id: string; owner: unknown }[]> {
+  const supabase = createClient(process.env.SUPABASE_PROJECT_URL!, process.env.SERVICE_ROLE_KEY!)
+  const { data, error } = await supabase.from('bids').select(`
+  id,
+  owner:  json->bid->bid_id->owner
+`)
+  if (error) {
+    throw error;
+  }
+  return data ?? [];
+}
+
+
