a56d5d2bb1ca768e73fd3fe9757ef4531387fd1d
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-12 14:11:10 +0300

    Refactor workspace settings, update ESLint configuration, and modify Deploy component in web/src/components/Deploy.tsx

--- web/src/components/Deploy.tsx
+++ web/src/components/Deploy.tsx
@@ -1,10 +1,10 @@
 import { Bid } from "@/types/bid";
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
 import axios from "axios";
 
 export const Deploy = () => {
     const { data : bids, isPending, error, mutateAsync: deploy } = useMutation({
-        mutationKey: ["deploy"],
+        mutationKey: ["Create_Deployment"],
         mutationFn: async (e: any) => {
             const response =  await axios.post<Bid[]>("http://localhost:3001/deploy");
             return response.data
@@ -14,16 +14,22 @@ export const Deploy = () => {
     if(isPending) return <div>Loading...</div>
 
     if(error) return <div>Error</div>
-     
 
     return (
-        <div>
-            <button onClick={deploy}>Deploy</button>
-          {bids && bids.map((bid) => (
-            <div key={bid.bid.bid_id.dseq}>
+        <div style={{display: "flex", flexDirection: "column", gap: "15px", padding: "10px", alignItems: "center"}}>
+            <button onClick={deploy}>Create Deployment</button>
+          {!bids || bids.length === 0 ? <h1>No Bids</h1> : 
+          <>
+            <h1>Bids</h1>
+          {bids.map((bid) => (
+            <div key={bid.bid.bid_id.dseq} style={{border: "1px solid black", display: "flex", gap: "15px", padding: "10px", alignItems: "center", width: "fit-content", borderRadius: "10px"}}>
               <h2>{bid.bid.price.amount}</h2>
+              <h2>{bid.bid.state}</h2>
+              <button> Accept Bid </button>
             </div>
           ))}
+        </>
+        }
         </div>
     )
 }
