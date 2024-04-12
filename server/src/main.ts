44af7a443231eff5adaa40e5413fdc864a2c9e59
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-12 21:57:23 +0300

    block requests only to our servers

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,8 @@ import { getAkashCoinPrice } from "./utils/price";
 const app = express();
 const port = 3001;
 
-app.use(cors());
+app.use(cors({ origin: ["https://akash-fgit-makyoyaho-tal-pivens-projects.vercel.app/", "http://localhost:5173"] }));
+
 app.use(express.json());
 
 app.use("/deploy", deployRouter);
