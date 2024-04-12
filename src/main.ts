28c7692db5482eedc7dee578cf74f3ae1d7a28f8
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-12 12:33:36 +0300

    minimal server

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,20 @@
 import "dotenv/config";
 import express from "express";
-import { Status } from "./type";
-import { getAllProvidesUrl, getClusterStatus } from "./utils";
+import { sendDeploy, walletFromMnemonic } from "./akash";
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const mnemonic =
+  "tragic wife grace victory volume journey base dirt calm morning lizard camera";
 
 app.listen(port, async () => {
   try {
-    const providersUrls = await getAllProvidesUrl();
-    const providersData: Array<Status> = [];
-    const batchSize = 20;
+    const wallet = await walletFromMnemonic(mnemonic);
+    const [account] = await wallet.getAccounts();
+    const deploy = await sendDeploy();
 
-    for (let i = 0; i < providersUrls.length; i += batchSize) {
-      const batchUrls = providersUrls.slice(i, i + batchSize);
-      const batchData = await Promise.all(
-        batchUrls.map((url) => getClusterStatus(url))
-      );
-      providersData.push(
-        ...batchData.filter((data): data is Status => data !== undefined)
-      );
-    }
-
-    console.log(providersData);
+    console.log(account, deploy);
   } catch (error: any) {
     console.error("Error: " + error);
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { ProvidersResponse, Status } from "./type";
 
+/**
+ * Get the URL of all the providers from akash-api.polkachu.com
+ * @returns The URL of all the providers
+ */
 export async function getAllProvidesUrl() {
   const providers = await axios.get<ProvidersResponse>(
     "https://akash-api.polkachu.com/akash/provider/v1beta3/providers"
@@ -8,15 +12,22 @@ export async function getAllProvidesUrl() {
   return providers.data.providers.map(({ host_uri }) => host_uri);
 }
 
+/**
+ * Get the status of a cluster with available resources from cloudmos.io
+ * @param providerUrl  The URL of the provider (domain + port)
+ * @returns  The status of the cluster with available resources
+ */
 export async function getClusterStatus(providerUrl: string) {
   try {
     const res = await axios.post<Status>("https://providerproxy.cloudmos.io/", {
-      url: "https://provider.xnets.club:8443" + "/status",
+      url: providerUrl + "/status",
       method: "GET",
     });
 
     return res.data;
   } catch (error: any) {
-    console.error("Error getting status for " + providerUrl + ": " + error);
+    console.error("Error getting status for: " + providerUrl);
   }
 }
+
+
