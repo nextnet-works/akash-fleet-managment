0e1499f9bf7f9f35c637bc75acf1aa7e4c41c386
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-05-10 12:10:03 +0300

    fix server

--- server/src/routes/deploy/index.ts
+++ server/src/routes/deploy/index.ts
@@ -8,60 +8,65 @@ const router = Router();
 const MAX_LEASES = 10;
 
 router.post("/create", async (req, res) => {
-  const deployment = req.body.body.deployment as Deployment;
+  try {
+    const deployment = req.body?.deployment as Deployment;
 
-  if (!deployment) {
-    return res.status(400).send("deployment is required");
-  }
-
-  const resourceUsed = DEPLOYMENT[deployment];
-
-  // TODO: save prices of each provider
-  const providerSupplies: ProviderSupply[] = [];
-
-  let isBidsEmpty = false;
-  let leasesResponses: SuccessfulLease[] = [];
-  let successfulLeaseCount = 0;
-  let ownerGen = "";
-  while (!isBidsEmpty) {
-    const { leasedAccepted, owner } = await handleSdlFlow();
-    ownerGen = owner;
-    if (leasedAccepted.length === 0) {
-      isBidsEmpty = true;
-      return;
+    if (!deployment) {
+      return res.status(400).send("deployment is required");
     }
 
-    for (const lease of leasedAccepted) {
-      if (successfulLeaseCount >= MAX_LEASES) {
+    const resourceUsed = DEPLOYMENT[deployment];
+
+    // TODO: save prices of each provider
+    const providerSupplies: ProviderSupply[] = [];
+
+    let isBidsEmpty = false;
+    let leasesResponses: SuccessfulLease[] = [];
+    let successfulLeaseCount = 0;
+    let ownerGen = "";
+    while (!isBidsEmpty) {
+      const { leasedAccepted, owner } = await handleSdlFlow();
+      ownerGen = owner;
+      if (leasedAccepted.length === 0) {
         isBidsEmpty = true;
         return;
       }
 
-      const providerIndex = providerSupplies.findIndex(
-        (provider) => provider.name === lease.provider
-      );
+      for (const lease of leasedAccepted) {
+        if (successfulLeaseCount >= MAX_LEASES) {
+          isBidsEmpty = true;
+          return;
+        }
 
-      if (providerIndex === -1) {
-        providerSupplies.push({ ...resourceUsed, name: lease.provider });
-      } else {
-        providerSupplies[providerIndex].cpu += resourceUsed.cpu;
-        providerSupplies[providerIndex].gpu += resourceUsed.gpu;
-        providerSupplies[providerIndex].memory += resourceUsed.memory;
-        providerSupplies[providerIndex].storage += resourceUsed.storage;
+        const providerIndex = providerSupplies.findIndex(
+          (provider) => provider.name === lease.provider
+        );
+
+        if (providerIndex === -1) {
+          providerSupplies.push({ ...resourceUsed, name: lease.provider });
+        } else {
+          providerSupplies[providerIndex].cpu += resourceUsed.cpu;
+          providerSupplies[providerIndex].gpu += resourceUsed.gpu;
+          providerSupplies[providerIndex].memory += resourceUsed.memory;
+          providerSupplies[providerIndex].storage += resourceUsed.storage;
+        }
+
+        successfulLeaseCount++;
+        leasesResponses = leasesResponses.concat(lease);
       }
-
-      successfulLeaseCount++;
-      leasesResponses = leasesResponses.concat(lease);
     }
+
+    const closedDeploymentsPromises = leasesResponses
+      .filter((lease) => lease.isSuccess)
+      .map((lease) => closeDeployment(lease.dseq, ownerGen));
+
+    await Promise.all(closedDeploymentsPromises);
+
+    res.status(201).json(providerSupplies);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal server error");
   }
-
-  const closedDeploymentsPromises = leasesResponses
-    .filter((lease) => lease.isSuccess)
-    .map((lease) => closeDeployment(lease.dseq, ownerGen));
-
-  await Promise.all(closedDeploymentsPromises);
-
-  res.status(201).json(providerSupplies);
 });
 
 export default router;
